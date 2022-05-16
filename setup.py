c37d2972c4993cfaaef9722d018487ee24496649
ui906879 <Bear11112222>
2022-05-16 11:21:26 +0100

    run black on setup.cfg

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description="common commodity plotting including seasonal charts using plotly",
     url="https://github.com/aeorxc/commodplot",
     project_urls={
-        'Source': 'https://github.com/aeorxc/commodplot',
+        "Source": "https://github.com/aeorxc/commodplot",
     },
     packages=setuptools.find_packages(),
     classifiers=[
@@ -16,8 +16,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['pandas', 'plotly', 'commodutil', 'cufflinks'],
-    python_requires='>=3.6',
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    install_requires=["pandas", "plotly", "commodutil", "cufflinks"],
+    python_requires=">=3.6",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
 )
