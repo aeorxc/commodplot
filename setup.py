9ea27d01d46d2c863be42d50838900e8ccfab7cd
ui906879 <Bear11112222>
2022-07-15 15:23:58 +0100

    fix: handle mergets where empty dataframe is passed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodplot",
-    version="1.4.9",
+    version="1.4.10",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity plotting including seasonal charts using plotly",
