45948ec0a1710bd8985b616797df94fc71d9dd68
ui906879 <gibran.afzal@rwe.com>
2025-05-09 16:11:26 +0100

    pandas compatibility fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodplot",
-    version="2.0.0",
+    version="2.0.1",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity plotting including seasonal charts using plotly",
